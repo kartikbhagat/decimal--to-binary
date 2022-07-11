0d9e16a3ad7ee1e6a46aef9741f565ed98e70e0f
kartikbhagat <109051564+kartikbhagat@users.noreply.github.com>
2022-07-11 08:11:21 +0530

    Add files via upload

---
+++ decimal_to_binary.c
@@ -0,0 +1,22 @@
+# include<stdio.h>
+
+int main() {
+    int n;
+    printf("Enter the decimal number: \n");
+    scanf("%d", &n);
+    int a[10];
+    int rem;
+    int i=0;
+
+    while(n){
+        rem=n%2;
+        n = n/2;
+        a[i]= rem;
+        i++;
+    }
+
+    for(int j= i-1; j>=0; j--){
+        printf("%d", a[j]);
+    }
+    return 0;
+}
